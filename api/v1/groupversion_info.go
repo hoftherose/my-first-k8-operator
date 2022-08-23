4b8940987854a42b598fd952d3f1f9622ae29b93
Héctor de la Rosa <hoftherose@gmail.com>
2022-08-23 12:55:56 -0500

    Make

--- api/v1/groupversion_info.go
+++ api/v1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1 contains API Schema definitions for the testing v1 API group
-//+kubebuilder:object:generate=true
-//+groupName=testing.example.com
+// +kubebuilder:object:generate=true
+// +groupName=testing.example.com
 package v1
 
 import (
